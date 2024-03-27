dc2f81817efabdf3c8e4d5ee9e9b3635c65fe740
YASEEN-FEROZ\mdyaseenferoz <mdyaseenferoz@outlook.com>
2024-03-27 08:39:55 +0530

    new db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://admin:8hUF4DSy7k2bvDnD@SG-yaseenfiroz-61957.servers.mongodirector.com:27017/admin', {
+mongoose.connect('mongodb://admin:8hUF4DSy7k2bvDnD@SG-yaseenfiroz-61957.servers.mongodirector.com:27017/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
