9c9ee4a65f35f6da1a7d5ff2db32d7659bf171c8
YASEEN-FEROZ\mdyaseenferoz <mdyaseenferoz@outlook.com>
2024-03-27 08:33:31 +0530

    try different mongo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://admin:8hUF4DSy7k2bvDnD@SG-yaseenfiroz-61957.servers.mongodirector.com:27017/admin?directConnection=true', {
+mongoose.connect('mongodb+srv://yaseenfiroz:tumkur45@cluster0.nnqznxh.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
